6dafc4a159cf66e6d3eeb8c4435de3572658138f
liuhuijie <liuhuijie@hongyunbang.net>
2018-03-06 10:37:22 +0800

    修改功能

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-    <vue-datepicker-local v-model="time" placeholder="请选择开始日期" format="YYYY-MM-DD HH:mm"  :disabled-date="disabledDate"  showButtons @confirm="confirm" clearable />
+    <VueDatePicker v-model="time" placeholder="请选择开始日期" format="YYYY-MM-DD HH:mm"  :disabled-date="disabledDate"  showButtons @confirm="confirm" clearable />
   </div>
 </template>
 
 <script>
-  import VueDatepickerLocal from './components/VueDatepickerLocal';
-
+import VueDatePicker from './components/date-picker/VueDatepickerLocal'
 export default {
   name: 'App',
   components: {
-    VueDatepickerLocal,
+    VueDatePicker
   },
+  data () {
+    return {
+      time: new Date()
+    }
+  },
+  methods: {
+    confirm () {
+      console.log(this.time)
+    },
+    disabledDate (time) {
+      var myDate = new Date()
+      return time > myDate.getTime()
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
